1d7954b37aab6ccd5388da8baf9e1fc6150f6acd
djingo-blip <74738367+djingo-blip@users.noreply.github.com>
2020-12-02 11:20:23 +0100

    Add files via upload

---
+++ Tag 1 Rätsel 1+2.py
@@ -0,0 +1,17 @@
+numbers = []
+
+with open("numbers.txt") as f:
+    numbers = list(map(int, f.readlines()))
+
+
+for x in numbers: 
+    for y in numbers:
+        if x+y == 2020:
+            print(x*y)
+
+for x in numbers: 
+    for y in numbers:
+        for z in numbers:
+            if x+y+z == 2020:
+                print(x*y*z)
+
